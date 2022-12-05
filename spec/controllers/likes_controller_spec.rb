a64d71ec5f63696e69de0113bd7acf305f25dca3
Owen Connor <owenconnor@gmail.com>
2022-12-05 15:48:15 +0000

    added likes_controller_spec.rb

---
+++ spec/controllers/likes_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe LikesController, type: :controller do
+  describe 'POST create' do
+    let(:valid_post_params) { { like: { liked_item_id: 1, liked_item_type: 'olio_item' } } }
+    let(:invalid_post_params) { { like: { liked_item_id: 1, liked_item_type: 'other_item' } } }
+
+    context 'with valid attributes' do
+      it 'creates a new like' do
+        expect{
+          post :create, params: valid_post_params
+        }.to change(Like, :count).by(1)
+      end
+
+      it 'redirects to the olio_items_index_path' do
+        post :create, params: valid_post_params
+        expect(response).to redirect_to(olio_items_index_path)
+        expect(flash[:success]).to match(/You successfully liked a product/)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the new like' do
+        expect{
+          post :create, params: invalid_post_params
+        }.to change(Like, :count).by(0)
+      end
+
+      it 'redirects to the olio_items_index_path' do
+        post :create, params: invalid_post_params
+        expect(response).to redirect_to(olio_items_index_path)
+        expect(flash[:notice]).to match(/We could not record your like*/)
+      end
+    end
+  end
+end
